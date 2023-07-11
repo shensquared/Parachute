6de4c79353f5c7612421713f8b0e8e9080f1b5fd
Shen Shen <shenshen@mit.edu>
2023-07-11 14:03:06 -0400

    title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Inter } from "@next/font/google";
+import Head from "next/head";
 
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
-
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -18,11 +18,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
+    <>
+      <Head><title>Self-hosted When2Meet | Shen Shen</title></Head>
     <main className={`${inter.variable} font-inter`}>
       <SessionProvider session={session} basePath='/w2m/api/auth'>
         <Component {...pageProps} />
       </SessionProvider>
     </main>
+    </>
   );
 };
 
