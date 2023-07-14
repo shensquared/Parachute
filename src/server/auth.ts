1da73c50a181b8a1493e783fb81678145ed3a571
Shen Shen <shenshen@mit.edu>
2023-07-14 01:35:16 -0400

    remove dependency

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,7 +7,7 @@ import {
 import Auth0Provider from "next-auth/providers/auth0";
 import GoogleProvider from "next-auth/providers/google";
 import GitlabProvider from "next-auth/providers/gitlab";
-import EmailProvider from "next-auth/providers/email";
+
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../env.mjs";
 import { prisma } from "./db";
